f51006a6ab9a94c10fda92eb6c8c93be2ad1117f
Yulia <penki@DESKTOP-MB89DTK>
2025-05-06 14:37:03 +0700

    konec zanytiy

---
+++ two.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 2;
+    float b = 3.14;
+    double c = 3.14159;
+    char j = 'd';
+    
+    printf("a = %d \n", a);
+    printf("b = %f \n", b);
+    printf("c = %lf \n", c);
+    printf("j = %c \n", j);
+    return 0;
+}
