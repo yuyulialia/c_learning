803a801f9fb0d5ea617f90b39152fa6e1df56598
Yulia <penki@DESKTOP-MB89DTK>
2025-05-18 22:27:39 +0700

    программа

---
+++ prog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void) {
+    int a = 4;
+    int b = 4;
+    if (a == b) {
+        printf("a == b"); 
+    }
+    else {
+        printf("a!=b");
+    }
+    return 0;
+}
